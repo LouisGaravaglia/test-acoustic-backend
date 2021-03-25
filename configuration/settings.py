7a577a8776d48ed1b6054227e6cfd3a683043381
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-25 16:15:05 -0700

    added a STATIC_ROOT path

--- configuration/settings.py
+++ configuration/settings.py
@@ -145,3 +145,9 @@ CSRF_HEADER_NAME = 'X-CSRFTOKEN'
 # Configure Django App for Heroku.
 # import django_heroku
 # django_heroku.settings(locals())
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
