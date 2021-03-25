64407219d844ab4b257acc20e4b97d4806c834b9
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-25 16:21:40 -0700

    added os to top

--- configuration/settings.py
+++ configuration/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,7 +147,7 @@ CSRF_HEADER_NAME = 'X-CSRFTOKEN'
 # import django_heroku
 # django_heroku.settings(locals())
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-import os
+
 DISABLE_COLLECTSTATIC=1
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
